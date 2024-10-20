5f417dfdef64c56d8f3d25dc02821ff71f0cbb79
Josef Frank <josef@kitsch.digital>
2024-10-21 03:28:06 +1100

    Create door logic

---
+++ modules/door/door.gd
@@ -0,0 +1,29 @@
+class_name Door
+extends Node3D
+## Door which probably starts locked.
+
+
+@export var goes_to: String = "<Room>"
+@export var logic_id: String = "<logic_id>"
+@export_node_path("Portal") var portal_path
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var useable: Useable = $Hinge/Useable
+
+
+func _ready() -> void:
+	useable.logic_id = logic_id
+	useable.title = "Door to %s" % goes_to
+
+	if portal_path:
+		var portal: Portal = get_node(portal_path)
+		portal.hide()
+
+
+func open() -> void:
+	# Swing that thang wide open.
+	animation_player.play("open")
+
+	if portal_path:
+		var portal: Portal = get_node(portal_path)
+		portal.show()
