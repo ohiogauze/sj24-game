8bc6035fe5af438bf7a2dbaed266e27fc7ebcf16
Josef Frank <josef@kitsch.digital>
2024-10-24 00:11:33 +1100

    Add gasp on loop start

--- rooms/01_house/states/intro.gd
+++ rooms/01_house/states/intro.gd
@@ -3,9 +3,14 @@ extends State
 
 const UseableScript = preload("res://classes/interactive/useable.gd")
 
+var gasp := AudioStreamPlayer.new()
+
 var first_entry = true
 
 func enter() -> void:
+	gasp.stream = preload("res://assets/sfx/waking_gasp.wav")
+	get_tree().root.add_child(gasp)
+	
 	var house: House = host
 
 	house.hud.state_machine.swap("Blank")
@@ -13,6 +18,9 @@ func enter() -> void:
 
 	if first_entry:
 		await get_tree().create_timer(1.0).timeout
+
+		gasp.play()
+		await get_tree().create_timer(2.0).timeout
 
 		var useable = Area3D.new()
 		useable.set_script(UseableScript)
